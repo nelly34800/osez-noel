6dee442f4ba0da9cab0010cbd3f050678cbab9f3
nelly34800 <nellyboussekane@free.fr>
2025-10-13 20:50:44 +0200

    page d'authentifications + contact

--- frontend/public/Router/allRoutes.js
+++ frontend/public/Router/allRoutes.js
@@ -3,6 +3,20 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signout", "Déconnexion", "/pages/auth/signout.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route(
+    "/editPassword",
+    "Changer mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
+  new Route(
+    "/editInfo",
+    "Modifier mes informations personnelles",
+    "/pages/auth/editInfo.html"
+  ),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Osez Noël";
