a9fc3f509886d7563d4eeff0aa94ab43bac7a755
nelly34800 <nellyboussekane@free.fr>
2025-10-18 17:09:51 +0200

    validation des champs d'inscription et gestion connexion/déconnexion et gestion des roles

--- frontend/public/Router/allRoutes.js
+++ frontend/public/Router/allRoutes.js
@@ -1,22 +1,13 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/contact", "Contact", "/pages/contact.html"),
-  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-  new Route("/signout", "Déconnexion", "/pages/auth/signout.html"),
-  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
-  new Route("/account", "Mon compte", "/pages/auth/account.html"),
-  new Route(
-    "/editPassword",
-    "Changer mot de passe",
-    "/pages/auth/editPassword.html"
-  ),
-  new Route(
-    "/editInfo",
-    "Modifier mes informations personnelles",
-    "/pages/auth/editInfo.html"
-  ),
+  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/contact", "Contact", "/pages/contact.html", []),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html",["disconnected"], "js/auth/signin.js"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html",["disconnected"], "js/auth/signup.js"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
+  new Route("/editPassword", "Changer mot de passe", "/pages/auth/editPassword.html", ["client"]),
+  new Route("/editInfo","Modifier mes informations personnelles","/pages/auth/editInfo.html", ["client"]),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Osez Noël";
